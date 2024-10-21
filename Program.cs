b87d98f5478fc98492dcc8ecc1a38a8e1e1cafec
Michael Resendez <michael.resendez1@hotmail.com>
2024-10-21 13:03:56 -0500

    added my 15 lines in txt

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LoopsExercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
